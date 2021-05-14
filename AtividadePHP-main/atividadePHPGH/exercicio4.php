e2f3c2fa13f00aeba004576d32d10921d888c952
guihbfs <83421261+guihbfs@users.noreply.github.com>
2021-05-14 16:01:26 -0300

    Add files via upload

---
+++ AtividadePHP-main/atividadePHPGH/exercicio4.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercício 4</title>
+</head>
+
+<body>
+
+<h1 style="color: blue;">VOCÊ É MAIS SAFADO(A) ou ANJO(A)? Descubra agora!</h1>
+
+<form action="exercicio4_index.php" method="GET">
+    <p> Digite a dia do seu nascimento: </p>
+    <input type="number" min="1" max="31" name="dia">
+    <p> Digite o mês do seu nascimento: </p>
+    <input type="number" min="1" max="12" name="mes">
+    <p> Digite o ano do seu nascimento: </p>
+    <input type="number" min="1" max="99" name="ano">
+
+    <input type="submit">
+</form>
+
+</body>
+
+</html>
---
+++ AtividadePHP-main/atividadePHPGH/exercicio4_visualizacao.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercício 4</title>
+</head>
+
+<body>
+
+<p>Sua porcentagem de SAFADEZA é: <?= $resultados_finais [0]; ?> % </p>
+<p>Sua porcentagem de ANJO é: <?= $resultados_finais[1]; ?> % </p>
+
+
+</body>
+
+</html>
